deeaa69203202cf0635644b9dc68477e5fc7b0db
minhnv <minhnv@kaopiz.com>
2024-11-13 20:44:24 +0700

    Update pages

--- lib/home/game_list_widget.dart
+++ lib/home/game_list_widget.dart
@@ -21,10 +21,7 @@ class _GameListWidgetState extends State<GameListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget(
-      tabletLayout: _buildTableWidget(),
-      mobileLayout: _buildMobileWidget(),
-    );
+    return _buildMobileWidget();
   }
 
   Widget _buildMobileWidget() {
@@ -35,138 +32,10 @@ class _GameListWidgetState extends State<GameListWidget> {
       itemBuilder: (context, index) {
         return Padding(
           padding: EdgeInsets.only(bottom: 20.sp),
-          child: _buildItemGameMobile(listApp[index], 100.sp),
+          child: _buildItemGameMobile(listApp[index], 150.sp),
         );
       },
       itemCount: listApp.length,
-    );
-  }
-
-  Widget _buildTableWidget() {
-    return GridView.builder(
-      padding: EdgeInsets.symmetric(vertical: 10.sp),
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: listApp.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 20.sp,
-        crossAxisSpacing: 20.sp,
-        childAspectRatio: 2.5 / 1,
-      ),
-      itemBuilder: (context, index) {
-        return _buildItemGame(listApp[index], 100.sp);
-      },
-    );
-  }
-
-  Widget _buildItemGame(AppModel appModel, double height) {
-    return Container(
-      height: height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.3),
-            offset: const Offset(0, 4),
-            blurRadius: 10,
-          ),
-        ],
-        borderRadius: BorderRadius.circular(5.sp),
-      ),
-      padding: EdgeInsets.all(5.sp),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset(
-              appModel.image,
-              width: 100.sp,
-              height: 100.sp,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    appModel.name,
-                    style: AppTextStyle.t20w700(),
-                  ),
-                  Text(
-                    "${context.l10n.download_count}: ${appModel.downloadCount}",
-                    style: AppTextStyle.t15w700(),
-                  ),
-                  SizedBox(height: 10.sp),
-                  Expanded(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: TextButton.icon(
-                            icon: Image.asset(
-                              AppImages.icPlayStore,
-                              width: 20.sp,
-                              height: 20.sp,
-                            ),
-                            onPressed: () async {
-                              if (appModel.linkPlayStore.isEmpty) {
-                                return;
-                              }
-                              if (await canLaunchUrl(
-                                Uri.parse(appModel.linkPlayStore),
-                              )) {
-                                await launchUrl(
-                                  Uri.parse(appModel.linkPlayStore),
-                                );
-                              }
-                            },
-                            label: Text(
-                              context.l10n.get,
-                              style: AppTextStyle.t8w700_underline(),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: TextButton.icon(
-                            icon: Image.asset(
-                              AppImages.icAppStore,
-                              width: 20.sp,
-                              height: 20.sp,
-                            ),
-                            onPressed: () async {
-                              if (appModel.linkAppStore.isEmpty) {
-                                return;
-                              }
-                              if (await canLaunchUrl(
-                                  Uri.parse(appModel.linkAppStore))) {
-                                await launchUrl(
-                                    Uri.parse(appModel.linkAppStore));
-                              }
-                            },
-                            label: Text(
-                              context.l10n.get,
-                              style: AppTextStyle.t8w700_underline(),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 
@@ -192,8 +61,6 @@ class _GameListWidgetState extends State<GameListWidget> {
           Expanded(
             child: Image.asset(
               appModel.image,
-              width: 100.sp,
-              height: 100.sp,
             ),
           ),
           Expanded(
@@ -216,60 +83,46 @@ class _GameListWidgetState extends State<GameListWidget> {
               ),
             ),
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: TextButton.icon(
-                    icon: Image.asset(
-                      AppImages.icPlayStore,
-                      width: 30.sp,
-                      height: 30.sp,
-                    ),
-                    onPressed: () async {
-                      if (appModel.linkPlayStore.isEmpty) {
-                        return;
-                      }
-                      if (await canLaunchUrl(
-                          Uri.parse(appModel.linkPlayStore))) {
-                        await launchUrl(Uri.parse(appModel.linkPlayStore));
-                      }
-                    },
-                    label: Text(
-                      context.l10n.get,
-                      style: AppTextStyle.t10w700(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: TextButton.icon(
-                    icon: Image.asset(
-                      AppImages.icAppStore,
-                      width: 30.sp,
-                      height: 30.sp,
-                    ),
-                    onPressed: () async {
-                      if (appModel.linkAppStore.isEmpty) {
-                        return;
-                      }
-                      if (await canLaunchUrl(
-                          Uri.parse(appModel.linkAppStore))) {
-                        await launchUrl(Uri.parse(appModel.linkAppStore));
-                      }
-                    },
-                    label: Text(
-                      context.l10n.get,
-                      style: AppTextStyle.t10w700(),
-                    ),
-                  ),
-                ),
-              ],
+          TextButton.icon(
+            icon: Image.asset(
+              AppImages.icPlayStore,
+              width: 50.sp,
+              height: 50.sp,
             ),
-          )
+            onPressed: () async {
+              if (appModel.linkPlayStore.isEmpty) {
+                return;
+              }
+              if (await canLaunchUrl(
+                  Uri.parse(appModel.linkPlayStore))) {
+                await launchUrl(Uri.parse(appModel.linkPlayStore));
+              }
+            },
+            label: Text(
+              context.l10n.get,
+              style: AppTextStyle.t20w700(),
+            ),
+          ),
+          TextButton.icon(
+            icon: Image.asset(
+              AppImages.icAppStore,
+              width: 50.sp,
+              height: 50.sp,
+            ),
+            onPressed: () async {
+              if (appModel.linkAppStore.isEmpty) {
+                return;
+              }
+              if (await canLaunchUrl(
+                  Uri.parse(appModel.linkAppStore))) {
+                await launchUrl(Uri.parse(appModel.linkAppStore));
+              }
+            },
+            label: Text(
+              context.l10n.get,
+              style: AppTextStyle.t20w700(),
+            ),
+          ),
         ],
       ),
     );
--- lib/home/social_media_widget.dart
+++ lib/home/social_media_widget.dart
@@ -20,16 +20,10 @@ class _SocialMediaWidgetState extends State<SocialMediaWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
+    return ListView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       itemCount: socialList.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
-        childAspectRatio: 2 / 1
-      ),
       itemBuilder: (context, index) {
         return _buildItemSocialMedia(socialList[index]);
       },
@@ -38,9 +32,12 @@ class _SocialMediaWidgetState extends State<SocialMediaWidget> {
 
   Widget _buildItemSocialMedia(SocialModel social) {
     return Container(
+      height: 100.sp,
+      padding: EdgeInsets.all(10.sp),
+      margin: EdgeInsets.only(bottom: 20.sp),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(10.sp),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.2),
@@ -49,31 +46,44 @@ class _SocialMediaWidgetState extends State<SocialMediaWidget> {
           ),
         ],
       ),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            flex: 3,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(100.sp),
-                  child: Image.asset(social.iconSocial, width: 50.sp, height: 50.sp,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                social.iconSocial,
+                width: 50.sp,
+                height: 50.sp,
+              ),
+              SizedBox(width: 20.sp),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100.sp),
+                child: Image.asset(
+                  social.image,
+                  width: 50.sp,
+                  height: 50.sp,
                 ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(100.sp),
-                  child: Image.asset(social.image, width: 50.sp, height: 50.sp),
-                )
-              ],
+              )
+            ],
+          ),
+          SizedBox(width: 20.sp),
+          Expanded(
+            child: Text(
+              social.name,
+              style: AppTextStyle.t15w700(),
             ),
           ),
-          Text(
-            social.name,
-            textAlign: TextAlign.center,
-            style: AppTextStyle.t15w700(),
-          ),
-          TextButton(
+          SizedBox(width: 20.sp),
+          TextButton.icon(
+            icon: Icon(
+              Icons.open_in_browser,
+              size: 50.sp,
+              color: Colors.black,
+            ),
             onPressed: () async {
               if (social.link.isEmpty) {
                 return;
@@ -82,7 +92,7 @@ class _SocialMediaWidgetState extends State<SocialMediaWidget> {
                 await launchUrl(Uri.parse(social.link));
               }
             },
-            child: Text(
+            label: Text(
               context.l10n.view,
               style: AppTextStyle.t15w700(),
             ),
